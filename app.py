0511db5588f8012c4b14f0178948b89fd8eff83f
kashyap-raksha <kraksha39@gmail.com>
2017-03-17 18:09:38 -0700

    latest commit

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
+import sys
 from flask import Flask
+from github import Github
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") #home page
 def hello():
     return "Hello from Dockerized Flask App!!"
 
+@app.route('/v1/<filename>')
+def display(filename):
+    argument = str(sys.argv[1])
+    url = argument.split("/")[-2:]
+    user_name = url[0]
+    repo_name = url[1]
+    git = Github("kashyap-raksha", "kashyap39")
+    repo = git.get_user(user_name).get_repo(repo_name)
+
+    if repo.name == "assignment1-config-example":
+        if filename.lower().endswith('.yml'):
+            if filename.lower() == "dev-config.yml":
+                contents = repo.get_file_contents('dev-config.yml')
+                return contents.decoded_content
+            elif filename.lower() == "test-config.yml":
+                contents = repo.get_file_contents('test-config.yml')
+                return contents.decoded_content
+
+        elif filename.lower().endswith('.json'):
+            if filename.lower() == "dev-config.json":
+                file_contents = repo.get_file_contents('dev-config.yml')
+                #contents = str("{\n\t"+file_contents+"\n}")
+                return "{"+file_contents.decoded_content+"}"
+
+            elif filename.lower() == "test-config.json":
+                contents = repo.get_file_contents('test-config.yml')
+                #json_obj = json.dumps(contents)
+                return "{"+(contents).decoded_content+"}"
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
